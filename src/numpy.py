4075ccf9bcd4123bb2966f6e91f4b749ce64ddf6
cttzs77 <carlos.hslima08@gmail.com>
2026-02-18 23:20:32 -0300

    Commit 06 - + Exercícios NumPy e começo Matplotlib

--- src/numpy.py
+++ src/numpy.py
@@ -99,3 +99,59 @@ print(conversion)
 
 # Print out product of np_baseball and conversion
 print(np_baseball * conversion)
+
+# ======================================================================================
+
+""" Average versus median
+You now know how to use numpy functions to get a better feeling for your data.
+
+The baseball data is available as a 2D numpy array with 3 columns (height, weight, age) and 1015 rows. The name of this numpy array is np_baseball. After restructuring the data, however, you notice that some height values are abnormally high. Follow the instructions and discover which summary statistic is best suited if you're dealing with so-called outliers. np_baseball is available.
+
+Instructions
+100 XP
+Create numpy array np_height_in that is equal to first column of np_baseball.
+Print out the mean of np_height_in.
+Print out the median of np_height_in. """
+
+# Create np_height_in from np_baseball
+np_height_in = np_baseball[:, 0]
+print(np_height_in)
+
+# Print out the mean of np_height_in
+print(np.mean(np_height_in))
+
+# Print out the median of np_height_in
+print(np.median(np_height_in))
+
+# ======================================================================================
+
+""" Explore the baseball data
+Because the mean and median are so far apart, you decide to complain to the MLB. They find the error and send the corrected data over to you. It's again available as a 2D NumPy array np_baseball, with three columns.
+
+The Python script in the editor already includes code to print out informative messages with the different summary statistics and numpy is already loaded as np. Can you finish the job? np_baseball is available.
+
+Instructions
+100 XP
+The code to print out the mean height is already included. Complete the code for the median height.
+Use np.std() on the first column of np_baseball to calculate stddev.
+Do big players tend to be heavier? Use np.corrcoef() to store the correlation between the first and second column of np_baseball in corr. """
+
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column
+heights = np_baseball[:, 0]
+weights = np_baseball[:, 1]
+corr = np.corrcoef(heights, weights)
+print("Correlation: " + str(corr))
+
+# ======================================================================================
+
